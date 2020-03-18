391816911260820afd230ef53c0d2af17192a70f
Philippe Daouadi <philippe.daouadi@tanker.io>
2020-03-18 15:15:35 +0100

    Add deprecated compatibility layer for options

---
+++ tanker-bindings/src/main/kotlin/io/tanker/api/EncryptOptions.kt
@@ -0,0 +1,4 @@
+package io.tanker.api
+
+@Deprecated("This class has been renamed", replaceWith = ReplaceWith("EncryptionOptions"))
+class EncryptOptions : EncryptionOptions()
---
+++ tanker-bindings/src/main/kotlin/io/tanker/api/ShareOptions.kt
@@ -0,0 +1,4 @@
+package io.tanker.api
+
+@Deprecated("This class has been renamed", replaceWith = ReplaceWith("SharingOptions"))
+class ShareOptions : SharingOptions()
