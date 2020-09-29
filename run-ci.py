09c04c7cc7f06085e036293292a9b61abfb4a2db
Théo DELRIEU <theo.delrieu@tanker.io>
2020-09-29 16:54:35 +0200

    ci: refactor run-ci.py

--- run-ci.py
+++ run-ci.py
@@ -1,10 +1,5 @@
-import argparse
-import os
-import sys
+from typing import List, Optional  # noqa
 
-from typing import List  # noqa
-
-from enum import Enum
 from path import Path
 import cli_ui as ui
 
@@ -16,7 +11,6 @@ import tankerci.git
 import tankerci.gcp
 import tankerci.gitlab
 
-LOCAL_TANKER = "tanker/dev@"
 
 PROFILES = [
     "gcc8-release-shared",
@@ -27,7 +21,9 @@ PROFILES = [
 ]
 
 
-def prepare(tanker_source: TankerSource, update: bool) -> None:
+def prepare(
+    tanker_source: TankerSource, update: bool, tanker_deployed_ref: Optional[str]
+) -> None:
     artifact_path = Path.getcwd() / "package"
     if tanker_source == TankerSource.UPSTREAM:
         profiles = [d.basename() for d in artifact_path.dirs()]
@@ -38,6 +34,7 @@ def prepare(tanker_source: TankerSource, update: bool) -> None:
         output_path=Path("tanker-bindings/conan"),
         profiles=profiles,
         update=update,
+        tanker_deployed_ref=tanker_deployed_ref,
     )
 
 
@@ -53,15 +50,17 @@ def test() -> None:
     )
 
 
-def build_and_test(args) -> None:
-    prepare(args.tanker_source, False)
+def build_and_test(
+    tanker_source: TankerSource, tanker_deployed_ref: Optional[str] = None
+) -> None:
+    prepare(tanker_source, False, tanker_deployed_ref)
     build()
     test()
 
 
 def deploy(*, version: str) -> None:
     tankerci.bump_files(version)
-    prepare(TankerSource.DEPLOYED, False)
+    prepare(TankerSource.DEPLOYED, False, None)
     build()
     test()
 
@@ -71,77 +70,25 @@ def deploy(*, version: str) -> None:
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--isolate-conan-user-home",
-        action="store_true",
-        dest="home_isolation",
-        default=False,
-    )
-    subparsers = parser.add_subparsers(title="subcommands", dest="command")
-
-    reset_branch_parser = subparsers.add_parser("reset-branch")
-    reset_branch_parser.add_argument("branch")
-
-    download_artifacts_parser = subparsers.add_parser("download-artifacts")
-    download_artifacts_parser.add_argument("--project-id", required=True)
-    download_artifacts_parser.add_argument("--pipeline-id", required=True)
-    download_artifacts_parser.add_argument("--job-name", required=True)
-
-    check_parser = subparsers.add_parser("build-and-test")
-    check_parser.add_argument(
-        "--use-tanker",
-        type=TankerSource,
-        default=TankerSource.EDITABLE,
-        dest="tanker_source",
-    )
-
-    prepare_parser = subparsers.add_parser("prepare")
-    prepare_parser.add_argument(
-        "--use-tanker",
-        type=TankerSource,
-        default=TankerSource.EDITABLE,
-        dest="tanker_source",
-    )
-    prepare_parser.add_argument(
-        "--update", action="store_true", default=False, dest="update",
-    )
-
-    deploy_parser = subparsers.add_parser("deploy")
-    deploy_parser.add_argument("--version", required=True)
-
-    subparsers.add_parser("mirror")
+    parser = tankerci.cpp.init_parser()
 
     args = parser.parse_args()
     if args.home_isolation:
         tankerci.conan.set_home_isolation()
-        tankerci.conan.update_config()
 
     if args.command == "build-and-test":
-        build_and_test(args)
-    elif args.command == "prepare":
-        prepare(
-            args.tanker_source, args.update,
+        build_and_test(
+            tanker_source=args.tanker_source,
+            tanker_deployed_ref=args.tanker_deployed_ref,
         )
+    elif args.command == "prepare":
+        prepare(args.tanker_source, args.update, args.tanker_deployed_ref)
     elif args.command == "deploy":
         deploy(version=args.version)
-    elif args.command == "reset-branch":
-        fallback = os.environ["CI_COMMIT_REF_NAME"]
-        ref = tankerci.git.find_ref(
-            Path.getcwd(), [f"origin/{args.branch}", f"origin/{fallback}"]
-        )
-        tankerci.git.reset(Path.getcwd(), ref)
-    elif args.command == "download-artifacts":
-        tankerci.gitlab.download_artifacts(
-            project_id=args.project_id,
-            pipeline_id=args.pipeline_id,
-            job_name=args.job_name,
-        )
     elif args.command == "mirror":
-        tankerci.git.mirror(github_url="git@github.com:TankerHQ/sdk-android")
+        tankerci.git.mirror(github_url="git@github.com:TankerHQ/sdk-ios")
     else:
-        parser.print_help()
-        sys.exit(1)
+        tankerci.cpp.handle_common_subcommands(parser, args)
 
 
 if __name__ == "__main__":
