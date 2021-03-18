c4bc98194d3cc8f52711e05d8564e6f4bf7ec0b7
Timothée Isnard <timothee.isnard@tanker.io>
2021-03-18 11:27:26 +0100

    chore(2fa): Rename withToken -> withSessionToken

--- tanker-bindings/src/main/kotlin/io/tanker/api/VerificationOptions.kt
+++ tanker-bindings/src/main/kotlin/io/tanker/api/VerificationOptions.kt
@@ -8,17 +8,17 @@ import com.sun.jna.Structure
 open class VerificationOptions: Structure() {
     // NOTE: Remember to keep the version in sync w/ the c++!
     @JvmField var version: Byte = 1
-    @JvmField var withToken: Byte = 0
+    @JvmField var withSessionToken: Byte = 0
 
     /**
     * Requests to create a Session Token on verification
     */
-    fun withToken(withToken: Boolean): VerificationOptions {
-        this.withToken = if (withToken) 1 else 0
+    fun withSessionToken(withSessionToken: Boolean): VerificationOptions {
+        this.withSessionToken = if (withSessionToken) 1 else 0
         return this
     }
 
     override fun getFieldOrder(): List<String> {
-        return listOf("version", "withToken")
+        return listOf("version", "withSessionToken")
     }
 }
