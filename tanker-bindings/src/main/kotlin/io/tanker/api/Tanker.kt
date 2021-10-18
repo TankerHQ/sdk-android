c810eeac998c10848f3a6cb1b93ec02a1daaca70
Tristan Floch <tristan.floch@tanker.io>
2021-10-18 09:04:56 +0000

    Revert "Merge branch 'tf/decrypt_v6' into 'master'"
    This reverts merge request !221

--- tanker-bindings/src/main/kotlin/io/tanker/api/Tanker.kt
+++ tanker-bindings/src/main/kotlin/io/tanker/api/Tanker.kt
@@ -391,9 +391,8 @@ class Tanker(tankerOptions: TankerOptions) {
 
         val outBuf = Memory(plainSize.coerceAtLeast(1))
         val futurePtr = lib.tanker_decrypt(tanker, outBuf, inBuf, data.size.toLong())
-        return TankerFuture<Pointer?>(futurePtr, Pointer::class.java).andThen(TankerCallbackWithKeepAlive(keepAlive = inBuf) {
-            val trueSize = Pointer.nativeValue(it)
-            outBuf.getByteArray(0, trueSize.toInt())
+        return TankerFuture<Unit>(futurePtr, Unit::class.java).andThen(TankerCallbackWithKeepAlive(keepAlive = inBuf) {
+            outBuf.getByteArray(0, plainSize.toInt())
         })
     }
 
