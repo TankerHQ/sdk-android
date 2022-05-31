f3b105bb517d81f1c8888240f0ba90f29d02e24e
Philippe Daouadi <philippe.daouadi@doctolib.com>
2022-05-31 16:31:56 +0200

    Admin: reimplement with okhttp

--- tanker-bindings/src/androidTest/kotlin/io/tanker/api/admin/Admin.kt
+++ tanker-bindings/src/androidTest/kotlin/io/tanker/api/admin/Admin.kt
@@ -1,12 +1,27 @@
 package io.tanker.api.admin
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.sun.jna.Pointer
-import io.tanker.api.TankerCallback
 import io.tanker.api.TankerFuture
 import io.tanker.api.TankerVoidCallback
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
+
+private fun toUrlSafeAppId(appId: String) = appId.replace('/', '_').replace('+', '-').trimEnd('=')
 
 class Admin(private val appManagementUrl: String, private val appManagementToken: String, private val apiUrl: String, private val environmentName: String) {
     private var cadmin: Pointer? = null
+    private val client = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val req = chain.request().newBuilder()
+                        .addHeader("authorization", "Bearer $appManagementToken")
+                        .build()
+                chain.proceed(req)
+            }
+            .build()
 
     companion object {
         private val lib = AdminLib.create()
@@ -32,24 +47,59 @@ class Admin(private val appManagementUrl: String, private val appManagementToken
     }
 
     fun createApp(name: String): TankerFuture<TankerApp> {
-        requireNotNull(cadmin) { "You need to connect() before using the app management API!" }
-        val cfut = lib.tanker_admin_create_app(cadmin!!, name)
-        return TankerFuture<Pointer>(cfut, Pointer::class.java, lib, keepAlive = this).andThen(TankerCallback {
-            val descriptor = TankerAppDescriptor(it)
-            TankerApp(apiUrl, descriptor.id!!, descriptor.authToken!!, descriptor.privateKey!!)
-        })
+        val jsonMapper = ObjectMapper()
+        val reqJson = jsonMapper.createObjectNode()
+        reqJson.put("environment_name", environmentName)
+        reqJson.put("name", name)
+
+        val request = Request.Builder()
+                .url("$appManagementUrl/v1/apps")
+                .method("POST", reqJson.toString().toRequestBody("application/json".toMediaType()))
+                .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+            val jsonResponse = jsonMapper.readTree(response.body?.string())
+            val jsonApp = jsonResponse.get("app")
+            return TankerFuture<Unit>(lib).then<TankerApp> { return@then TankerApp(client, apiUrl, jsonApp.get("id").asText(), jsonApp.get("auth_token").asText(), jsonApp.get("private_key").asText()) }
+        }
     }
 
     fun deleteApp(appId: String): TankerFuture<Unit> {
-        requireNotNull(cadmin) { "You need to connect() before using the app management API!" }
-        return TankerFuture(lib.tanker_admin_delete_app(cadmin!!, appId), Unit::class.java, lib, keepAlive = this)
+        val request = Request.Builder()
+                .url("$appManagementUrl/v1/apps/${toUrlSafeAppId(appId)}")
+                .method("DELETE", null)
+                .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+            return TankerFuture(lib)
+        }
     }
 
     /**
      * Updates the app properties
      */
     fun appUpdate(appId: String, options: TankerAppUpdateOptions): TankerFuture<Unit> {
-        requireNotNull(cadmin) { "You need to connect() before using the app management API!" }
-        return TankerFuture(lib.tanker_admin_app_update(cadmin!!, appId, options), Unit::class.java, lib, keepAlive = this)
+        val jsonMapper = ObjectMapper()
+        val reqJson = jsonMapper.createObjectNode()
+        if (options.oidcClientId != null)
+            reqJson.put("oidc_client_id", options.oidcClientId)
+        if (options.oidcClientProvider != null)
+            reqJson.put("oidc_provider", options.oidcClientProvider)
+        if (options.preverifiedVerification != null)
+            reqJson.put("preverified_verification_enabled", options.preverifiedVerification?.value != 0.toByte())
+        if (options.userEnrollment != null)
+            reqJson.put("enroll_users_enabled", options.userEnrollment?.value != 0.toByte())
+
+        val request = Request.Builder()
+                .url("$appManagementUrl/v1/apps/${toUrlSafeAppId(appId)}")
+                .method("PATCH", reqJson.toString().toRequestBody("application/json".toMediaType()))
+                .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+            return TankerFuture(lib)
+        }
     }
 }
--- tanker-bindings/src/androidTest/kotlin/io/tanker/api/admin/TankerApp.kt
+++ tanker-bindings/src/androidTest/kotlin/io/tanker/api/admin/TankerApp.kt
@@ -1,35 +1,58 @@
 package io.tanker.api.admin
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.sun.jna.Pointer
 import io.tanker.api.TankerFuture
 import io.tanker.bindings.TankerLib
 import io.tanker.api.TankerCallback
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
 
-class TankerApp(private val url: String, val id: String, val authToken: String, val privateKey: String) {
+class TankerApp(private val client: OkHttpClient, private val url: String, val id: String, val authToken: String, val privateKey: String) {
     companion object {
         private val lib = AdminLib.create()
         private val tankerlib = TankerLib.create()
     }
 
-
     fun getEmailVerificationCode(email: String): TankerFuture<String> {
-        val fut = TankerFuture<Pointer>(lib.tanker_get_email_verification_code(url, id, authToken, email), Pointer::class.java, lib, keepAlive = this)
-        return fut.then(TankerCallback {
-            val ptr = it.get()
-            val str = ptr.getString(0)
-            tankerlib.tanker_free_buffer(ptr)
-            str
-        })
+        val jsonMapper = ObjectMapper()
+        val reqJson = jsonMapper.createObjectNode()
+        reqJson.put("app_id", id)
+        reqJson.put("auth_token", authToken)
+        reqJson.put("email", email)
+
+        val request = Request.Builder()
+                .url("$url/verification/email/code")
+                .method("POST", reqJson.toString().toRequestBody("application/json".toMediaType()))
+                .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+            val jsonResponse = jsonMapper.readTree(response.body?.string())
+            return TankerFuture<Unit>(lib).then<String> { return@then jsonResponse.get("verification_code").asText() }
+        }
     }
 
     fun getSMSVerificationCode(phoneNumber: String): TankerFuture<String> {
-        val fut = TankerFuture<Pointer>(lib.tanker_get_sms_verification_code(url, id, authToken, phoneNumber), Pointer::class.java, lib, keepAlive = this)
-        return fut.then(TankerCallback {
-            val ptr = it.get()
-            val str = ptr.getString(0)
-            tankerlib.tanker_free_buffer(ptr)
-            str
-        })
+        val jsonMapper = ObjectMapper()
+        val reqJson = jsonMapper.createObjectNode()
+        reqJson.put("app_id", id)
+        reqJson.put("auth_token", authToken)
+        reqJson.put("phone_number", phoneNumber)
+
+        val request = Request.Builder()
+                .url("$url/verification/sms/code")
+                .method("POST", reqJson.toString().toRequestBody("application/json".toMediaType()))
+                .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+            val jsonResponse = jsonMapper.readTree(response.body?.string())
+            return TankerFuture<Unit>(lib).then<String> { return@then jsonResponse.get("verification_code").asText() }
+        }
     }
 }
 
