f580e2451f6cb5b683008e9010229a6304020e42
Timothée Isnard <timothee.isnard@tanker.io>
2024-01-11 19:26:19 +0100

    refactor(libs.versions.toml): Avoid unnecessary references & harmonize
    We keep a few version references for version numbers that are
    reused in multiple entries (JNA) or particularly important.
    It's otherwise not a good thing, as having every library split in
    two under different names just makes it harder to maintain.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 }
 
 dependencies {
-    implementation("net.java.dev.jna:jna-platform:5.13.0")
-    implementation("com.github.pgreze:kotlin-process:1.4.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation(libs.jna.platform)
+    implementation(libs.pgreze.kotlin.process)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.serialization.json)
 }
