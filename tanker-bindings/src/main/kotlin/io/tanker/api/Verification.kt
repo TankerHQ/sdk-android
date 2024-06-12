41c0d0f34cb2fd83dc20a49f6a8b9c92c802cc5a
Quentin Vernot <quentin.vernot@doctolib.com>
2024-06-12 12:59:30 +0200

    chore(core): deprecate old OIDC flow

--- tanker-bindings/src/main/kotlin/io/tanker/api/Verification.kt
+++ tanker-bindings/src/main/kotlin/io/tanker/api/Verification.kt
@@ -4,6 +4,7 @@ sealed class Verification
 data class EmailVerification(val email: String, val verificationCode: String) : Verification()
 data class PassphraseVerification(val passphrase: String) : Verification()
 data class VerificationKeyVerification(val verificationKey: String) : Verification()
+@Deprecated("The entire OIDC flow has been reworked in version 4.2.0, 'OIDCIDTokenVerification' has been deprecated as a result, use 'OIDCAuthorizationCodeVerification' instead")
 data class OIDCIDTokenVerification(val oidcIDToken: String) : Verification()
 data class PhoneNumberVerification(val phoneNumber: String, val verificationCode: String) : Verification()
 data class PreverifiedEmailVerification(val preverifiedEmail: String) : Verification()
